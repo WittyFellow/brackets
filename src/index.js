0eae468f3b8d0328c7cdfd60783eafbd6d8b17c0
Anton <ant.roma24@gmail.com>
2021-02-15 12:54:07 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let bracketsStr = bracketsConfig[j].join("");
+      while (str.includes(bracketsStr)) {
+        str = str.replace(bracketsStr, "");
+      }
+    }
+  }
+  return str.length === 0 ? true : false;
 }
